deb2663c9d7c3626c7f7de4671fc79c96b589a24
Philipp Specht <philippspecht1990@gmail.com>
2020-09-03 19:08:31 +0200

    changes

--- projects/social_network/src/database/friendships.sql
+++
@@ -1,9 +0,0 @@
-DROP TABLE IF EXISTS friendships CASCADE;
-
-CREATE TABLE friendships(
-  id SERIAL PRIMARY KEY,
-  sender_id INT NOT NULL REFERENCES users(id),
-  receiver_id INT NOT NULL REFERENCES users(id),
-  accepted BOOLEAN DEFAULT false,
-  created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
-);
